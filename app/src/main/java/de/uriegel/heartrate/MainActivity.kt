b20f80015c88b214db3de5e7bc3f3469ee6666e1
uriegel <uriegel@hotmail.de>
2021-09-11 13:52:01 +0200

    Permission Location, for background too

--- app/src/main/java/de/uriegel/heartrate/MainActivity.kt
+++ app/src/main/java/de/uriegel/heartrate/MainActivity.kt
@@ -1,11 +1,39 @@
 package de.uriegel.heartrate
 
+import android.Manifest
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import de.uriegel.activityextensions.ActivityRequest
+import de.uriegel.activityextensions.async.delay
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        launch {
+            val result = activityRequest.checkAndAccessPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION))
+            if (result.any { it.value == false }) {
+                Toast.makeText(this@MainActivity, "Kein Zugriff auf den Standort", Toast.LENGTH_LONG).show()
+                return@launch
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                val backgroundResult = activityRequest.checkAndAccessPermissions(arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION))
+                if (backgroundResult.any { it.value == false }) {
+                    Toast.makeText(this@MainActivity, "Kein ständiger Zugriff auf den Standort", Toast.LENGTH_LONG).show()
+                    return@launch
+                }
+            }
+        }
     }
+
+    override val coroutineContext = Dispatchers.Main
+
+    val activityRequest = ActivityRequest(this)
 }
